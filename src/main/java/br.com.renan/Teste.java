149a29f13bdd893c4d19933f844fe8a5e9da2503
Renan Henrique Peres <renanperes@finchsolucoes.com.br>
2019-02-07 11:54:01 -0200

    first commit

---
+++ src/main/java/br.com.renan/Teste.java
@@ -0,0 +1,9 @@
+package br.com.renan;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Teste {
+    public static void main(String args[]) {
+        System.out.println(new BCryptPasswordEncoder().encode("123456"));
+    }
+}
